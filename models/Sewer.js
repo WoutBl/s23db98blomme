4dcc4a003f850dcb7dc0fd0ae056df523e79afaf
BlommeWout <wout.blomme@student.howest.be>
2023-04-27 19:23:57 -0500

    added validation to model

--- models/Sewer.js
+++ models/Sewer.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose")
 const sewerSchema = mongoose.Schema({
-_id:Number,
-Length: Number,
-Material: String,
-Location: String
+    _id: Number,
+    Length: { type: Number, set: v => Math.floor(v), default: 10, min: 1, max: 100 },
+    Material: String,
+    Location: String
 })
 module.exports = mongoose.model("Sewer",
-sewerSchema)
+    sewerSchema)
